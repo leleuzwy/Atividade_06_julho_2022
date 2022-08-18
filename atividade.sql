216490bdf87f599f9647794379f08ff1d8535e41
leléu <108069111+leleuzwy@users.noreply.github.com>
2022-08-18 08:59:28 -0300

    incluindo conteúdo na tabela

--- atividade.sql
+++ atividade.sql
@@ -1,23 +1,47 @@
-Create Table students(
-Student_id INT
-first name VAR CHAR (45)
-last name VAR CHAR (45)
-phone VAR CHAR (20)
-email VAR CHAR (80)
-videochat_user VAR CHAR (45)
-comonts TEXT
+    
+CREATE TABLE students (
+    student_id INT,
+    first_name VARCHAR(45),
+    last_name VARCHAR(45),
+    phone VARCHAR(20),
+    email VARCHAR(20),
+    videochat_user VARCHAR(20),
+    comments TEXT
+);
+CREATE TABLE students_classes (
+    student_id INT,
+    class_id INT
+);
+CREATE TABLE classes (
+    class_id INT,
+    max_students TINYINT,
+    teacher_id INT,
+    subject_id INT
+);
+CREATE TABLE teachers(
+    teacher_id INT,
+    first_name VARCHAR(45),
+    last_name VARCHAR(45),
+    phone VARCHAR(20),
+    email VARCHAR(20)
+);
+CREATE TABLE teachers_subjects (
+    teacher_id INT,
+    subject_id INT
 );
 
-Create Table students_classes(students_id INT
-class_id INT
+CREATE TABLE class_hours_classes (
+    class_hours_id INT,
+    class_id INT
 );
-
-Create Table  class_hours_classes(class_hours_id INT 
-class_id INT
+CREATE TABLE class_hours (
+    class_hours_id INT,
+    currentweekday ENUM(...),
+    time_start TIME,
+    time_end TIME
 );
-
-Create Table class_hours(class_hours_id INT
-weekday ENUM (....)
-time_start TIME
-time_end TIME 
+CREATE TABLE subjects (
+    subject_id INT,
+    username VARCHAR(45),
+    currentlevel VARCHAR(45)
 );
